53a251faef877c2e2c0d79f7c05cd23fc5cf9884
Vibhor Vats <vibhor.stav@gmail.com>
2022-01-06 14:53:41 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/vibhorvats/com/destiniii/MainActivity.kt
@@ -0,0 +1,6 @@
+package vibhorvats.com.destiniii
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,9 @@
+import 'story_brain.dart';
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({this.storyTitle = '', this.choice1 = '', this.choice2 = ''});
+}
